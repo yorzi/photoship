d5c756db57f66aeb7eb4636a91f72199803d500d
Andy Wang <wangyaodi@gmail.com>
2017-02-01 21:15:37 +0800

    add flickr api related links/materials in flickr.rb

--- config/initializers/flickr.rb
+++ config/initializers/flickr.rb
@@ -1,2 +1,7 @@
+# GEM: https://github.com/hanklords/flickraw
+# Search API: https://www.flickr.com/services/api/flickr.photos.search.html
+# Search Web Sandbox: https://www.flickr.com/services/api/explore/flickr.photos.search
+# URL Patterns: https://www.flickr.com/services/api/misc.urls.html
+
 FlickRaw.api_key = ENV['FLICKR_KEY']
 FlickRaw.shared_secret = ENV['FLICKR_SECRET']
