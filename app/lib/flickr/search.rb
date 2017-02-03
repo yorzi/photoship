c3636d2babfc62609710747b39bb48bf0a30b380
Andy Wang <wangyaodi@gmail.com>
2017-02-03 18:01:02 +0800

    fix a tiny issue related to cache key

--- app/lib/flickr/search.rb
+++ app/lib/flickr/search.rb
@@ -3,7 +3,7 @@ module Flickr
     attr_reader :list
 
     def initialize(options={})
-      @list = Rails.cache.fetch "searched_photos_#{options[:keywords]}##{options[:page]}", expires_in: 10.minutes do
+      @list = Rails.cache.fetch "searched_photos_#{options[:text]}##{options[:page]}", expires_in: 10.minutes do
         flickr.photos.search(options)
       end
     end
