cae5e9f28455f240a2e94700a9f782354ad196c0
Andy Wang <wangyaodi@gmail.com>
2017-02-04 09:44:05 +0800

    add loading spinner effect while scroll window to the bottom

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $(window).scroll(function() {
     if($(window).scrollTop() == $(document).height() - $(window).height()) {
       console.log("scrolled... loading data from: " + $("#photos").data('links'));
+      $('.loading').show();
       $.get($("#photos").data('links'));
     }
   });
